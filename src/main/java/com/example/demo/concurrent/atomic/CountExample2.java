1d71bbe427e8e05037eec36e6337aa5d0c0e9dcd
doublemow <17600567862@163.com>
2018-12-11 21:44:41 +0800

    a

---
+++ src/main/java/com/example/demo/concurrent/atomic/CountExample2.java
@@ -0,0 +1,46 @@
+package com.example.demo.concurrent.atomic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class CountExample2 {
+
+    // 请求总数
+    public static  int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static  int threadTotal = 200;
+
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i = 0; i < clientTotal; i++){
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("InterruptedException",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        log.info("count:{}", count);
+    }
+
+    private static void add(){
+        count.incrementAndGet();
+        // count.getAndIncrement();
+    }
+}
