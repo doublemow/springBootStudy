6d2c5cb87d5fb37b8fcedd43cc72dde30b9b2c48
chenqi <17600567862@163.com>
2018-12-18 15:53:29 +0800

    a

--- src/main/java/com/example/demo/algorithms/SingleLinkList.java
+++ src/main/java/com/example/demo/algorithms/SingleLinkList.java
@@ -2,16 +2,23 @@ package com.example.demo.algorithms;
 
 /**
  * 单链表
- * 主要算法：判断是否是回文
+ * 主要算法：
+ * 判断是否是回文
+ * 单链表反转
+ * 判断是否成环
+ * 两个有序链表合并
+ * 删除链表第n 个节点
+ * 寻找链表中间节点
+ *
  * @author chen qi
  */
 public class SingleLinkList {
 
-    public Node head = null;
-    public int n = 0;
-    public static final int CACHE_CAPACITY = 20;
+    private Node head = null;
+    private int n = 0;
+    private static final int CACHE_CAPACITY = 20;
 
-    public Node findByValue(int value){
+    private Node findByValue(int value){
         Node p = head;
         while(p != null && p.data != value){
             p = p.next;
@@ -19,7 +26,7 @@ public class SingleLinkList {
         return p;
     }
 
-    public Node findByIndex(int index){
+    private Node findByIndex(int index){
         Node p  = head;
         int n = 0;
         while(p != null && n < index){
@@ -29,7 +36,18 @@ public class SingleLinkList {
         return p;
     }
 
-    public void insertToHead(Node newNode){
+    private Node findTail(){
+        if(head ==  null){
+            return null;
+        }
+        Node p = head;
+        while (p.next != null){
+            p = p.next;
+        }
+        return p;
+    }
+
+    private void insertToHead(Node newNode){
         if(head == null){
             head = newNode;
         }else{
@@ -38,7 +56,13 @@ public class SingleLinkList {
         }
     }
 
-    public void insertTail(int value){
+    private void insertTailAll(int[] values){
+        for(int value : values){
+            insertTail(value);
+        }
+    }
+
+    private void insertTail(int value){
         Node tailNode = new Node(value, null);
 
         if(head == null){
@@ -164,10 +188,9 @@ public class SingleLinkList {
 
     /**
      * 翻转链表(从头到p的位置)
-     * @param p
-     * @return
+     * @param p 结束节点
      */
-    public Node inverseLinkList(Node p){
+    private Node inverseLinkList(Node p){
         Node pre = null;
         Node r = head;
         Node next;
@@ -183,13 +206,11 @@ public class SingleLinkList {
         return r;
     }
 
+
     /**
      * 判断两个链表中的值是否一致
-     * @param left
-     * @param right
-     * @return
      */
-    public boolean TFResult(Node left, Node right){
+    private boolean TFResult(Node left, Node right){
         Node l = left;
         Node r = right;
         System.out.println("left:" + l.data);
@@ -206,6 +227,62 @@ public class SingleLinkList {
         }else {
             return false;
         }
+    }
+
+    /**
+     * 寻找链表中间节点
+     * @return node 链表节点
+     */
+    private Node findCentreNode(){
+        Node p = head;
+        Node pre = head;
+        if(p == null){
+            return null;
+        }
+        while(pre.next != null && pre.next.next != null){
+            p = p.next;
+            pre = pre.next.next;
+        }
+        return p;
+    }
+
+    /**
+     * 环状检测
+     */
+    private boolean checkCircle(){
+        if(head == null){
+            return false;
+        }
+
+        Node p = head;
+        Node q = head;
+
+        do {
+            p = p.next;
+            q = q.next.next;
+            if(p == q){
+                return true;
+            }
+        }while (q.next != null && q.next.next != null);
+
+        return false;
+    }
+
+    private boolean checkCircle1(){
+        if(head == null){
+            return false;
+        }
+
+        Node p = head;
+
+        while(p != null){
+            p = p.next;
+            if(p == head){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -228,7 +305,7 @@ public class SingleLinkList {
             return;
         }
         Node p = head;
-        Node q = null;
+        Node q;
         while (p.next != null){
             if(p.next.data == value){
                 q = p.next;
@@ -264,9 +341,7 @@ public class SingleLinkList {
         //判断是否是回文
         SingleLinkList link = new SingleLinkList();
         int[] data = {1,2,5,2,1};
-        for(int i =0; i < data.length; i++){
-            link.insertTail(data[i]);
-        }
+        link.insertTailAll(data);
         System.out.println("打印原始:");
         link.printAll();
         if (link.palindrome()){
@@ -276,6 +351,55 @@ public class SingleLinkList {
         }
         //判断结束
 
+        //寻找中间节点
+        SingleLinkList link1 = new SingleLinkList();
+        int[] data1 = {1,2,3,2,1};
+        link1.insertTailAll(data1);
+        System.out.println("寻找中间节点开始，原始数据为：");
+        link1.printAll();
+        Node centre = link1.findCentreNode();
+        if(centre == null){
+            System.out.println("链表为空无中间节点");
+        }else{
+            System.out.println("中间节点：" + centre.getData());
+        }
+        //end
+
+        //单链表反转
+        SingleLinkList link2 = new SingleLinkList();
+        int[] data2 = {1,2,3,4,5};
+        link2.insertTailAll(data2);
+        System.out.println("单链表反转,原始数据：");
+        link2.printAll();
+        Node tail = link2.findTail();
+        Node r = link2.inverseLinkList(tail);
+        System.out.println("反转后结果：");
+        while(r != null){
+            System.out.println(r.getData());
+            r = r.next;
+        }
+        //end
+
+        //判断是否成环
+        System.out.println("检测单链表是否是环");
+        SingleLinkList link3 = new SingleLinkList();
+        link3.head = createNode(1);
+        Node p1 = createNode(2);
+        Node p2 = createNode(3);
+        link3.head.next = p1;
+        p1.next = p2;
+        System.out.println("检测结果：" + link3.checkCircle());
+        System.out.println("检测结果：" + link3.checkCircle1());
+        //end
+
+        //两个有序链表合并
+
+
+        //end
+
+        //删除链表第n 个节点
+
+        //end
     }
 
 }
