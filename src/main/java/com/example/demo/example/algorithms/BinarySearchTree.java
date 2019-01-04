4069069d29e3868ff49d116993dba21da5cefebb
chenqi <17600567862@163.com>
2019-01-04 13:36:03 +0800

    a

---
+++ src/main/java/com/example/demo/example/algorithms/BinarySearchTree.java
@@ -0,0 +1,10 @@
+package com.example.demo.example.algorithms;
+
+public class BinarySearchTree {
+
+     public  static  class Node{
+         int data;
+         Node left;
+         Node right;
+     }
+}
