995fb3073409ae93bfe2d29d066620be10aab238
doublemow <17600567862@163.com>
2019-01-20 21:57:38 +0800

    a

--- src/main/java/com/example/demo/example/concurrent/atomic/ExAtomicInteger.java
+++ src/main/java/com/example/demo/example/concurrent/atomic/ExAtomicInteger.java
@@ -6,18 +6,23 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.*;
 
 @Slf4j
 public class ExAtomicInteger {
 
     // 请求总数
-    public static  int clientTotal = 5000;
+    private static int clientTotal = 5000;
 
     // 同时并发执行的线程数
-    public static  int threadTotal = 200;
+    private static  int threadTotal = 200;
 
-    public static AtomicInteger count = new AtomicInteger(0);
+    private static AtomicInteger count = new AtomicInteger(0);
+    private static AtomicLong atomicLong = new AtomicLong(0L);
+    private static AtomicBoolean atomicBoolean = new AtomicBoolean(true);
+    private static AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(6);
+    private static LongAdder longAdder = new LongAdder();
+    private static DoubleAdder doubleAdder = new DoubleAdder();
 
     public static void main(String[] args) throws Exception{
         ExecutorService executorService = Executors.newCachedThreadPool();
@@ -39,10 +44,16 @@ public class ExAtomicInteger {
         }
         countDownLatch.await();
         log.info("count:{}", count);
+        log.info("atomicLong:{}",atomicLong);
     }
 
     private static void add(){
         count.incrementAndGet();
+        atomicLong.incrementAndGet();
+        atomicBoolean.compareAndSet(true,false);
+        atomicIntegerArray.incrementAndGet(1);
+        longAdder.increment();
+        doubleAdder.add(1.0D);
         // count.getAndIncrement();
     }
 }
